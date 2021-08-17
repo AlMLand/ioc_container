8c2941f0b4c80a826eb0b1cea78375788197724c
AlMLand <morlandalex@googlemail.com>
2021-08-18 00:11:51 +0200

    Create a client class with a demo method

---
+++ src/main/java/lightweight_ioc_container/ioc_container/service/ComponentClient.java
@@ -0,0 +1,22 @@
+package lightweight_ioc_container.ioc_container.service;
+
+import lightweight_ioc_container.ioc_container.annotation.Bean;
+import lightweight_ioc_container.ioc_container.annotation.Inject;
+import lightweight_ioc_container.ioc_container.annotation.Named;
+
+@Bean
+public class ComponentClient {
+
+	@Inject
+	private ComponentAlphaService componentAlphaService;
+	@Inject
+	@Named("componentBetaServiceImpl")
+	private ComponentBetaService componentBetaService;
+	
+	public void showAllComponentNames() {
+		String componentAlphaServiceName = componentAlphaService.getComponentAlphaName();
+		String componentBetaServiceName = componentBetaService.getComponentBetaName();
+		System.out.printf("Alpha name: %s\nBeta name: %s", componentAlphaServiceName, componentBetaServiceName);
+	}
+	
+}
