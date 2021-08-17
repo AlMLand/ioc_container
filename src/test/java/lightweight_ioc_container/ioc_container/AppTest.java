34d3bff082fe97b457aa1d893beb2f53532ad075
AlMLand <morlandalex@googlemail.com>
2021-08-17 17:02:08 +0200

    Started a project, added a .gitignore file

---
+++ src/test/java/lightweight_ioc_container/ioc_container/AppTest.java
@@ -0,0 +1,20 @@
+package lightweight_ioc_container.ioc_container;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
