4a2ed14a7bce36cf8fe2bdb7085fdae04b3d26e1
AlMLand <morlandalex@googlemail.com>
2021-08-18 00:00:01 +0200

    Create a ComponentAlphaServiceImpl class

---
+++ src/main/java/lightweight_ioc_container/ioc_container/service/ComponentAlphaServiceImpl.java
@@ -0,0 +1,13 @@
+package lightweight_ioc_container.ioc_container.service;
+
+import lightweight_ioc_container.ioc_container.annotation.Bean;
+
+@Bean
+public class ComponentAlphaServiceImpl implements ComponentAlphaService {
+
+	@Override
+	public String getComponentAlphaName() {
+		return "I am a Alpha";
+	}
+
+}
