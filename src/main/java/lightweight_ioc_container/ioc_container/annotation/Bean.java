98b477a48cf22526c1842dc3dd7d1e0cc3fd4d26
AlMLand <morlandalex@googlemail.com>
2021-08-17 23:43:08 +0200

    Create @Bean annotaion

---
+++ src/main/java/lightweight_ioc_container/ioc_container/annotation/Bean.java
@@ -0,0 +1,13 @@
+package lightweight_ioc_container.ioc_container.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Bean {
+
+}
