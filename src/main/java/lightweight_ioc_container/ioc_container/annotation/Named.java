345a6cf603232e81c0ca7a2788ebe24a269e26bd
AlMLand <morlandalex@googlemail.com>
2021-08-17 23:49:28 +0200

    Create @Named annotation

---
+++ src/main/java/lightweight_ioc_container/ioc_container/annotation/Named.java
@@ -0,0 +1,19 @@
+package lightweight_ioc_container.ioc_container.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ TYPE, FIELD, METHOD, PARAMETER, ANNOTATION_TYPE })
+public @interface Named {
+	
+	String value() default "";
+
+}
