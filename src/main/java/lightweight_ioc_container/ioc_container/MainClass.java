228f5ab0cf1d25539c70facebeedc4c26aa3cb9b
AlMLand <morlandalex@googlemail.com>
2021-08-18 03:40:54 +0200

    Create the MainClass

---
+++ src/main/java/lightweight_ioc_container/ioc_container/MainClass.java
@@ -0,0 +1,16 @@
+package lightweight_ioc_container.ioc_container;
+
+import lightweight_ioc_container.ioc_container.customframework.CustomInjector;
+import lightweight_ioc_container.ioc_container.service.ComponentClient;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		CustomInjector.startApplication(MainClass.class);
+		ComponentClient componentClient = CustomInjector.getService(ComponentClient.class);
+		componentClient.showAllComponentNames();
+		
+	}
+
+}
