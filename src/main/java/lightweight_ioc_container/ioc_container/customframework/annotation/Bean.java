3d2325ac81f98efce00f05074624301a44b9e7f7
AlMLand <morlandalex@googlemail.com>
2021-08-18 22:32:42 +0200

    Added a comment to the class

--- src/main/java/lightweight_ioc_container/ioc_container/customframework/annotation/Bean.java
+++ src/main/java/lightweight_ioc_container/ioc_container/customframework/annotation/Bean.java
@@ -1,5 +1,8 @@
 package lightweight_ioc_container.ioc_container.customframework.annotation;
 
+/**
+ * Mark class as 'bean'
+ */
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
