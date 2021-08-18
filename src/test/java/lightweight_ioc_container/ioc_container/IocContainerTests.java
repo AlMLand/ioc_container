16a578707d103be76a876e8b2e07d634a96e5514
AlMLand <morlandalex@googlemail.com>
2021-08-18 23:48:15 +0200

    Added tests

--- src/test/java/lightweight_ioc_container/ioc_container/IocContainerTests.java
+++ src/test/java/lightweight_ioc_container/ioc_container/IocContainerTests.java
@@ -1,22 +1,22 @@
 package lightweight_ioc_container.ioc_container;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import lightweight_ioc_container.ioc_container.customframework.CustomInjector;
+import lightweight_ioc_container.ioc_container.service.ComponentAlphaService;
+import lightweight_ioc_container.ioc_container.service.ComponentBetaService;
 import lightweight_ioc_container.ioc_container.service.ComponentClient;
 
 public class IocContainerTests {
 	
-	private ComponentClient componentClient;
-	
 	@Before
 	public void setUp() {
 		CustomInjector.startApplication(MainClass.class);
-		componentClient = CustomInjector.getService(ComponentClient.class);
 	}
 	
 	@After
@@ -26,12 +26,37 @@ public class IocContainerTests {
 	
 	@Test
     public void shouldAnswerWithTrueWhenComponentAlphaCallMethodGetComponentAlphaName() {
+		ComponentClient componentClient = CustomInjector.getService(ComponentClient.class);
 		assertEquals("I am a Alpha", componentClient.getComponentAlphaService().getComponentAlphaName());
     }
 	
 	@Test
     public void shouldAnswerWithTrueWhenComponentBetaCallMethodGetComponentBetaName() {
+		ComponentClient componentClient = CustomInjector.getService(ComponentClient.class);
 		assertEquals("I am a Beta", componentClient.getComponentBetaService().getComponentBetaName());
     }
+	
+	@Test
+	public void shouldReturnTrueAlphaServiceNotNullAndAlphaServiceCallGetComponentAlphaName() {
+		ComponentAlphaService componentAlphaService = CustomInjector.getService(ComponentAlphaService.class);
+		assertNotNull(componentAlphaService);
+		assertEquals("I am a Alpha", componentAlphaService.getComponentAlphaName());
+	}
+	
+	@Test
+	public void shouldReturnTrueBetaServiceNotNullAndBetaServiceCallGetComponentBetaName() {
+		ComponentBetaService componentBetaService = CustomInjector.getService(ComponentBetaService.class);
+		assertNotNull(componentBetaService);
+		assertEquals("I am a Beta", componentBetaService.getComponentBetaName());
+	}
+	
+	@Test
+	public void shouldReturnTrueClient_1NotNullAndClient_2NotNullAndClient_1EqualsClient_2() {
+		ComponentClient client_1 = CustomInjector.getService(ComponentClient.class);
+		assertNotNull(client_1);
+		ComponentClient client_2 = CustomInjector.getService(ComponentClient.class);
+		assertNotNull(client_2);
+		assertEquals(client_1, client_2);
+	}
 	
 }
